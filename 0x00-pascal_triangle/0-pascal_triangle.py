d08f325461db5440fb63bd3dc114ea4e160885c0
George Raafat <georgexraafat@gmail.com>
2024-01-03 17:58:35 +0200

    doing some task

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """prints pascal triangle with hight n"""
 
+
 def pascal_triangle(n):
     """prints pascal triangle with hight n"""
     if (n <= 0):
@@ -8,7 +9,7 @@ def pascal_triangle(n):
     ans = [[1]]
     for i in range(n-1):
         lst = [1]
-        for j in range (i):
+        for j in range(i):
             lst.append(ans[i][j] + ans[i][j+1])
         lst.append(1)
         ans.append(lst)
