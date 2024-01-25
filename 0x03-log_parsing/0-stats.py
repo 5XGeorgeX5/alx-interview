2becad8be174e6e16551fefc640b68d9da9bc8b1
George Raafat <georgexraafat@gmail.com>
2024-01-25 11:48:40 +0200

    doing some task

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -8,11 +8,13 @@ status_codes = {}
 valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
 count = 0
 
+
 def print_stats(size, status_codes):
     """Print accumulated metrics."""
     print("File size: {}".format(size))
     for key in sorted(status_codes):
         print(f"{key}: {status_codes[key]}")
+
 
 try:
     for line in sys.stdin:
