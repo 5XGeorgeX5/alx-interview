78287e48485763649cece6d3e4e40e1bc6d9a9a4
George Raafat <georgexraafat@gmail.com>
2024-02-15 21:40:55 +0200

    doing some task

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const request = require("request");
+
+const API = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
+
+request(API, (err, response, body) => {
+  if (!err) {
+    const charAPI = JSON.parse(body).characters;
+    print(charAPI, 0);
+  }
+});
+
+function print(charAPI, i) {
+  request(charAPI[i], (err, response, body) => {
+    if (!err) {
+      const char = JSON.parse(body);
+      console.log(char.name);
+      if (i + 1 < charAPI.length) {
+        print(charAPI, i + 1);
+      }
+    }
+  });
+}
