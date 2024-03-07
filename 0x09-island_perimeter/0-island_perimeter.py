d0c32f7b45c073ae6b159dfb8ab34685f6fff27b
George Raafat <georgexraafat@gmail.com>
2024-03-07 23:20:48 +0200

    doing some task

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines an island perimeter measuring function."""
+
+
+def island_perimeter(grid):
+    """returns the perimeter of the island described in grid."""
+    rows, cols, perimeter = len(grid), len(grid[0]), 0
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4 - 2 * (grid[i - 1][j] + grid[i][j - 1])
+    return perimeter
